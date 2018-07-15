0b4667d356c00c709657334ae2a540d38a7a886d
Vipul Modi <vipulm@microsoft.com>
2018-07-15 16:40:26 -0700

    remove fireworks demo from this repo for now until it is stable.

--- src/fireworks/publish.linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker tag azure-mesh-fireworks-web:dev-alpine vipulmmsft/azure-mesh-fireworks-web:0.1-alpine
-docker push vipulmmsft/azure-mesh-fireworks-web:0.1-alpine
-docker tag azure-mesh-fireworks-worker-v1:dev-strech vipulmmsft/azure-mesh-fireworks-worker-v1:0.1-strech
-docker push vipulmmsft/azure-mesh-fireworks-worker-v1:0.1-strech
-docker tag azure-mesh-fireworks-worker-v2:dev-strech vipulmmsft/azure-mesh-fireworks-worker-v2:0.1-strech
-docker push vipulmmsft/azure-mesh-fireworks-worker-v2:0.1-strech
