8700dbc0f01b4fb9833c9da80417120e3893c39f
Mikkel Mork Hegnhoj <mikhegn@microsoft.com>
2018-08-21 23:40:49 +0200

    Fixed VotingAppforLinux deployment template

--- src/votingapp/linux/deploy-azure.sh
+++ src/votingapp/linux/deploy-azure.sh
@@ -5,5 +5,5 @@ location=$2
 #az login
 #az account set --subscription {your subscriptionID}
 
-az group create --name $rgname --location $location
-az mesh deployment create --resource-group $rgname --template-file ./Deployment/quickstart-linux.json
+az group create --name $rgName --location $location
+az mesh deployment create --resource-group $rgName --template-file ./Deployment/quickstart-linux.json
