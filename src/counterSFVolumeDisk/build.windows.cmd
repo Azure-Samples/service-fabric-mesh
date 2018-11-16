cd4a4ec898953855c0f2a7e9abd9fdc068fdf850
Ashish Negi <asnegi@microsoft.com>
2018-11-16 12:29:50 +0530

    Remove duplicate code packages in counterSFVolumeDisk

--- src/counterSFVolumeDisk/build.windows.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-echo docker build src -f src\counterService\Dockerfile -t azure-mesh-counter:dev-nanoserver-1709
-docker build src -f src\counterService\Dockerfile -t azure-mesh-counter:dev-nanoserver-1709
