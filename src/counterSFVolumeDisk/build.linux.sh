28a7f0634b749033518ea9423e801902faeb900d
Ashish Negi <ashishnegi33@gmail.com>
2018-11-20 10:47:57 +0530

    Flush data to disk in counter app and remove duplicate code (#48)
    * Flush data to disk.
    
    * Remove duplicate code packages in counterSFVolumeDisk

--- src/counterSFVolumeDisk/build.linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo docker build src -f src/counterService/linux.Dockerfile -t azure-mesh-counter:dev-alpine
-docker build src -f src/counterService/linux.Dockerfile -t azure-mesh-counter:dev-alpine
