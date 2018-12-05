65308ac12f157eb0a1abeeee69f06c1244f6aec8
Tyler Whitney <TylerMSFT@users.noreply.github.com>
2018-12-04 16:02:06 -0800

    update app to new tooling (#60)
    * update app to new tooling

--- src/todolistapp/WebFrontEnd/Program.cs
+++ src/todolistapp/WebFrontEnd/Program.cs
@@ -19,6 +19,7 @@ namespace WebFrontEnd
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
